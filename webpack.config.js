12afbc433dd4c457783b2fb3623cfdda0b4b8e49
Devon Kiss <tuffs@gmx.com>
2018-07-15 01:41:19 -0500

    Added file-loader to dev dependencies and included test case for image formats to support in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,26 +11,30 @@ module.exports = {
 	module: {
 		rules: [
 			{ test: /\.js$/, use: 'babel-loader' },
-			  {
-    test: /\.(scss)$/,
-    use: [{
-      loader: 'style-loader', // inject CSS to page
-    }, {
-      loader: 'css-loader', // translates CSS into CommonJS modules
-    }, {
-      loader: 'postcss-loader', // Run post css actions
-      options: {
-        plugins: function () { // post css plugins, can be exported to postcss.config.js
-          return [
-            require('precss'),
-            require('autoprefixer')
-          ];
-        }
-      }
-    }, {
-      loader: 'sass-loader' // compiles Sass to CSS
-    }]
-  },
+			{ test: /\.(scss)$/,
+		    use: [{
+		      loader: 'style-loader', // inject CSS to page
+		    }, {
+		      loader: 'css-loader', // translates CSS into CommonJS modules
+		    }, {
+		      loader: 'postcss-loader', // Run post css actions
+		      options: {
+		        plugins: function () { // post css plugins, can be exported to postcss.config.js
+		          return [
+		            require('precss'),
+		            require('autoprefixer')
+		          ];
+		        }
+		      }
+		    },{
+		      loader: 'sass-loader' // compiles Sass to CSS
+		    }]
+		  },
+		  { test: /\.(png|svg|jpg|gif)$/,
+       	use: [
+        	 'file-loader'
+       	]
+       }
 		]
 	},
 	devServer: {
